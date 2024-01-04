f8375bd4d2db1fb08b01ac1b00bad6e94939be51
Khoa Nguyen <nhnkhoa@live.com>
2024-01-04 12:02:32 +0000

    site config

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://3edesign.vercel.app",
   i18n: {
     defaultLocale: "vi",
     locales: ["vi", "en"],
